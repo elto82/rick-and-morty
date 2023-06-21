29d16e2089ece80f033b263ca106003d5f2748ca
Argiro <elto.82@gmai.com>
2023-06-21 15:28:56 -0500

    puerto sin definir

--- back/src/DB_connection.js
+++ back/src/DB_connection.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 const modelCharacter = require("./models/Character");
 const modelFavorite = require("./models/Favorite.js");
 
